bacf42b1bd258fc9bc0d7974a2cb33f733350f4b
hlop3z <23062270+hlop3z@users.noreply.github.com>
2020-04-01 15:41:29 -0500

    update the repository

--- amdam/__main__.py
+++ amdam/__main__.py
@@ -6,8 +6,8 @@ import socket
 import fcntl
 import struct
 import psutil
-#from . import api
-import api
+from . import api
+#import api
 
 from quart import Quart, websocket, request, render_template, send_from_directory, send_file
 from quart_cors import cors
